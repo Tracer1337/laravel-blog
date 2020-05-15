11d7f428171df71b951b6558765776ac1a0599f1
Merlin Moelter <joebau.0815@googlemail.com>
2020-05-15 16:22:25 +0200

    Fix cookie consent

--- resources/js/react/components/Dialog/templates/CookieConsent.js
+++ resources/js/react/components/Dialog/templates/CookieConsent.js
@@ -35,7 +35,7 @@ const CookieConsent = ({ onAccept }) => {
             <p>We use cookies and other tracking technologies to improve your browsing experience on our website, to show you personalized content and targeted ads, to analyze our website traffic, and to understand where our visitors are coming from. By browsing our website, you consent to our use of cookies and other tracking technologies.</p>
 
             <div className="actions">
-                <button onClick={handleAccept}>Accept</button>
+                <button onClick={() => handleAccept()}>Accept</button>
                 <button onClick={handlePreferences}>Change Preferences</button>
             </div>
         </div>
