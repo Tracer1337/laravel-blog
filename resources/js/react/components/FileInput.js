86302bfb7c45800a6265a99fe899f0f8733eaa23
Merlin Moelter <joebau.0815@googlemail.com>
2020-05-20 22:46:16 +0200

    Show and Validate filesize client sided

--- resources/js/react/components/FileInput.js
+++ resources/js/react/components/FileInput.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react"
 import { useFormContext } from "react-hook-form"
 
 import Icon from "./Icon.js"
+import Dialog from "./Dialog/Dialog.js"
+
+import { MAX_FILE_UPLOAD_SIZE_MB } from "../config/constants.js"
 
 let idCounter = 0
 
@@ -10,13 +13,21 @@ const FileInput = ({ accept, onChange, icon, name, useHooks, className, ...props
 
     const [id] = useState(idCounter++)
     const [label, setLabel] = useState(props.label)
+    const [size, setSize] = useState()
 
     const htmlId = "file_upload_" + id
 
     const handleChange = event => {
         const file = event.target.files[0]
+
+        if(file.size / 1024 / 1024 > MAX_FILE_UPLOAD_SIZE_MB) {
+            Dialog.error(`The file is too large (Max: ${MAX_FILE_UPLOAD_SIZE_MB}MB)`)
+            return
+        }
+
         onChange?.(file, name)
         setLabel(file.name || props.label)
+        setSize(file.size)
 
         if(useHooks) {
             formHooks.setValue(name, file)
@@ -41,6 +52,9 @@ const FileInput = ({ accept, onChange, icon, name, useHooks, className, ...props
 
             <label htmlFor={htmlId}>
                 {icon && <Icon type={icon} className="icon"/>}
+
+                {size && <span className="size">{Math.round(size / 1024 / 1024 * 100) / 100}MB</span>}
+
                 <span>{label}</span>
             </label>
         </div>
--- resources/js/react/config/constants.js
+++ resources/js/react/config/constants.js
@@ -1,2 +1,3 @@
 export const isMobile = window.innerWidth <= 1000
 export const GATrackingId = "UA-162994094-1"
+export const MAX_FILE_UPLOAD_SIZE_MB = 2
