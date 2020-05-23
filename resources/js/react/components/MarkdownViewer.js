bdc4a24312faacee21663053435a35970ef296cd
Merlin Moelter <joebau.0815@googlemail.com>
2020-05-23 11:59:38 +0200

    Add syntax highlighting

--- resources/js/react/components/MarkdownViewer.js
+++ resources/js/react/components/MarkdownViewer.js
@@ -1,12 +1,29 @@
 import React from "react"
 import Markdown from "react-markdown"
+import hljs from "highlight.js/lib/core"
+
+import javascript from "highlight.js/lib/languages/javascript"
+import scss from "highlight.js/lib/languages/scss"
+import xml from "highlight.js/lib/languages/xml"
+import python from "highlight.js/lib/languages/python"
+import php from "highlight.js/lib/languages/php"
+
+import "highlight.js/styles/github.css"
+
+// Define syntax highlighting
+hljs.registerLanguage("javascript", javascript)
+hljs.registerLanguage("scss", scss)
+hljs.registerLanguage("xml", xml)
+hljs.registerLanguage("python", python)
+hljs.registerLanguage("php", php)
 
 const getWidth = str => str.match(/w:([^,]*)/)?.[1]
 const getHeight = str => str.match(/h:([^,]*)/)?.[1]
 
+// Define custom renderers
 const renderers = {
     image: ({ src, alt }) => {
-        // Extract dimensions
+        // Render image with size extracted from alt prop
         let width, height
         if(alt) {
             width = getWidth(alt)
@@ -14,6 +31,16 @@ const renderers = {
         }
 
         return <img src={src} style={{ width, height }}/>
+    },
+
+    code: ({ language, value }) => {
+        const formatted = hljs.highlight(language, value)
+
+        return (
+            <pre>
+                <code dangerouslySetInnerHTML={{ __html: formatted.value }}/>
+            </pre>
+        )
     }
 }
 
@@ -22,6 +49,8 @@ const MarkdownViewer = (props) => {
         <Markdown
             {...props}
             renderers={renderers}
+            escapeHtml={false}
+            className="markdown"
         />
     )
 }
