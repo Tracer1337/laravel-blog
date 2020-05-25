6386c4c692feb5c351ebe2c79a496c873315144e
Merlin Moelter <joebau.0815@googlemail.com>
2020-05-25 16:03:10 +0200

    Comment component

--- resources/js/react/components/Blogpost/Comments.js
+++ resources/js/react/components/Blogpost/Comments.js
@@ -1,29 +1,18 @@
 import React from "react"
 import Skeleton from "react-loading-skeleton"
 
-import CommentActions from "./CommentActions.js"
-import Date from "../Date.js"
+import CommentCard from "../CommentCard.js"
 
 const Comments = ({ comments, onAction }) => (
     <div className="comments">
         <h3 className="title">Comments</h3>
 
         {comments ? comments.map(comment => (
-            <div className="card comment" key={comment.id} data-id={comment.id}>
-                <div className="comment-head">
-                    <div className="username">{comment.user.full_name}</div>
-                    <div className="dates">
-                        {comment.created_at !== comment.updated_at && (
-                            <div className="edited-hint">Edited: <Date timestamp={comment.updated_at} /></div>
-                        )}
-                        <Date timestamp={comment.created_at}/>
-                    </div>
-                </div>
-                
-                <div>{comment.content}</div>
-
-                <CommentActions comment={comment} onAction={onAction}/>
-            </div>
+            <CommentCard
+                data={comment}
+                onAction={onAction}
+                key={comment.id}
+            />
         )) : <Skeleton count={3} height={100}/>}
     </div>
 )
---
+++ resources/js/react/components/CommentCard.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { connect } from "react-redux"
+import { Link } from "react-router-dom"
+
+import Dialog from "./Dialog/Dialog.js"
+import Icon from "./Icon.js"
+import Date from "./Date.js"
+
+import { deleteComment } from "../config/API.js"
+import { gaEvent } from "../utils/GATracking.js"
+
+const mapStateToProps = store => ({
+    profile: store.auth.profile
+})
+
+const CommentActions = connect(mapStateToProps)(({ data, profile, onAction }) => {
+    if (data.user_id !== profile.id) {
+        return null
+    }
+
+    const handleDelete = async () => {
+        const shouldDelete = await Dialog.verify("The comment will be removed. This action cannot be undone!")
+        if (shouldDelete) {
+            gaEvent({
+                category: "Comment",
+                action: "Delete"
+            })
+
+            await deleteComment(data.id)
+            onAction()
+        }
+    }
+
+    return (
+        <div className="actions">
+            <Link to={`/post/${data.blogpost_id}?commentId=${data.id}&editComment=true`}>
+                <Icon type="edit" className="icon" />
+            </Link>
+            <Icon type="delete" className="icon" onClick={handleDelete} />
+        </div>
+    )
+})
+
+const CommentCard = ({ data, onAction }) => (
+    <div className="card comment" data-id={data.id}>
+        <div className="comment-head">
+            <Link to={"/user/" + data.user.id} className="wrapper-link">
+                <div className="username">{data.user.full_name}</div>
+            </Link>
+            <div className="dates">
+                {data.created_at !== data.updated_at && (
+                    <div className="edited-hint">Edited: <Date timestamp={data.updated_at} /></div>
+                )}
+                <Date timestamp={data.created_at} />
+            </div>
+        </div>
+
+        <div>{data.content}</div>
+
+        <CommentActions data={data} onAction={onAction} />
+    </div>
+)
+
+export default CommentCard
