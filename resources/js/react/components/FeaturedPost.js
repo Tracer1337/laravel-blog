1795ccc0c5a1a4f73399ea57901a854f4ab808d4
Merlin Moelter <joebau.0815@googlemail.com>
2020-05-26 16:00:40 +0200

    Remove title from featured post component

--- resources/js/react/components/FeaturedPost.js
+++ resources/js/react/components/FeaturedPost.js
@@ -12,25 +12,21 @@ const FeaturedPost = () => {
     }
 
     return (
-        <section className="featured-post-wrapper">
-            <h3 className="title">Featured Post</h3>
-
-            <div className="featured-post">
-                <div className="text-container">
-                    <div className="head">
-                        <div className="blogpost-title">{data ? data.blogpost.title : <Skeleton width={500}/>}</div>
-                        <div className="username">{data ? data.blogpost.user.full_name : <Skeleton width={300}/>}</div>
-                    </div>
-
-                    <div className="content">{data ? data.content : <Skeleton height={200}/>}</div>
+        <section className="featured-post">
+            <div className="text-container">
+                <div className="head">
+                    <div className="blogpost-title">{data ? data.blogpost.title : <Skeleton width={500}/>}</div>
+                    <div className="username">{data ? data.blogpost.user.full_name : <Skeleton width={300}/>}</div>
                 </div>
 
-                <div className="image-container">
-                    <div className="inner-image-container">
-                        <Link to={data ? "/post/" + data.blogpost.id : ""}>
-                            {data ? <img alt="cover" src={data.blogpost.cover?.url}/> : <Skeleton height={500}/>}
-                        </Link>
-                    </div>
+                <div className="content">{data ? data.content : <Skeleton height={200}/>}</div>
+            </div>
+
+            <div className="image-container">
+                <div className="inner-image-container">
+                    <Link to={data ? "/post/" + data.blogpost.id : ""}>
+                        {data ? <img alt="cover" src={data.blogpost.cover?.url}/> : <Skeleton height={500}/>}
+                    </Link>
                 </div>
             </div>
         </section>
