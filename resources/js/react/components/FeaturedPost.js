5d8c1ca1896e40434f83e1f9180b565f879b8aeb
Merlin Moelter <joebau.0815@googlemail.com>
2020-05-26 16:34:11 +0200

    Responsive design for featured post

--- resources/js/react/components/FeaturedPost.js
+++ resources/js/react/components/FeaturedPost.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 import Skeleton from "react-loading-skeleton"
 
 import useAPIData from "../utils/useAPIData.js"
+import { isMobile } from "../config/constants.js"
 
 const FeaturedPost = () => {
     const [data, reload, setData, isLoading] = useAPIData({ method: "getFeaturedPost" })
@@ -12,24 +13,28 @@ const FeaturedPost = () => {
     }
 
     return (
-        <section className="featured-post">
-            <div className="text-container">
-                <div className="head">
-                    <div className="blogpost-title">{data ? data.blogpost.title : <Skeleton width={500}/>}</div>
-                    <div className="username">{data ? data.blogpost.user.full_name : <Skeleton width={300}/>}</div>
+        <>
+            <section className="featured-post">
+                <div className="text-container">
+                    <div className="head">
+                        <div className="blogpost-title">{data ? data.blogpost.title : <Skeleton width={500}/>}</div>
+                        <div className="username">{data ? data.blogpost.user.full_name : <Skeleton width={300}/>}</div>
+                    </div>
+
+                    <div className="content">{data ? data.content : <Skeleton height={200}/>}</div>
                 </div>
 
-                <div className="content">{data ? data.content : <Skeleton height={200}/>}</div>
-            </div>
-
-            <div className="image-container">
-                <div className="inner-image-container">
-                    <Link to={data ? "/post/" + data.blogpost.id : ""}>
-                        {data ? <img alt="cover" src={data.blogpost.cover?.url}/> : <Skeleton height={500}/>}
-                    </Link>
+                <div className="image-container">
+                    <div className="inner-image-container">
+                        <Link to={data ? "/post/" + data.blogpost.id : ""}>
+                            {data ? <img alt="cover" src={data.blogpost.cover?.url}/> : <Skeleton height={500}/>}
+                        </Link>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+
+            {isMobile && <hr/>}
+        </>
     )
 }
 
